2430bcfc115654ff6e3a5d1223b872e05dccad06
Eric George <eric@pokelondon.com>
2015-08-03 09:45:06 +0100

    rotation, translte and scale looks ok. might need a bit more data to the south

--- script.js
+++ script.js
@@ -1,26 +1,76 @@
-var width = 960,
-    height = 1160;
+    var width = 1200,
+    height = 1200;
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var projection = d3.geo.orthographic()
+        .translate([width / 2, height / 2])
+        .center([0.0, 0.0])
+        .scale(450);
 
-var projection = d3.geo.mercator()
-    .scale(500)
-    .translate([width / 2, height / 2]);
+    var path = d3.geo.path()
+        .projection(projection);
 
-var path = d3.geo.path()
-    .projection(projection);
+    var λ = d3.scale.linear()
+        .domain([0, 100])
+        .range([-180, 180]);
+
+    var φ = d3.scale.linear()
+        .domain([0, 100])
+        .range([90, -90]);
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    function rotate(a, b) {
+        projection.rotate([λ(a), φ(b)]);
+        svg.selectAll("path").attr("d", path);
+    };
 
 
-d3.json("londontopo.json", function(error, data) {
-    if (error) return console.error(error);
-    console.log(data);
+    var buildings = svg.append("g");
 
-    // Convert TopoJSON to GeoJSON for display
-    var geoJSON = topojson.feature(data, data.objects.london);
+    d3.json("londontopo.json", function(error, data) {
+        if (error) throw error;
 
-    svg.append("path")
-        .datum(geoJSON)
+        data.transform = {
+            scale: [0.00005736413748528683, 0.000017385988039137747],
+            translate: [-0.190389659968519, 51.43136245091755]
+        };
+
+        scale = function(scalefactor) {
+            return [scalefactor * 0.005736413748528683, scalefactor * 0.0028385988039137747];
+        }
+        data.transform = {
+            scale: scale(4),
+            translate: [20, -40]
+        };
+
+    // Convert to GeoJSON for rendering
+    var buildingsGeoJSON = topojson.feature(data, data.objects.london);
+
+    buildings.append("path")
+        .datum(buildingsGeoJSON)
+        .attr("class", "buildings")
         .attr("d", path);
-});
+
+    //landuse.append("path")
+        //.datum(topojson.object(data, data.objects.landuse))
+        //.attr("class", "landuse")
+        //.attr("d", path);
+
+    });
+
+    rotate(15, 50);
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var rotateInput = document.getElementById('rotate_input');
+        var rotateElement = document.getElementById('rotate_value');
+
+        rotateInput.addEventListener('mouseup', function(evt) {
+            rotateElement.innerHTML = rotateInput.value;
+            rotate(rotateInput.value, 50);
+        });
+    });
+
+
+
